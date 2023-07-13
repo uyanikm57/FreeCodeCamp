322cde3fa8970b85f83889130ba7ffc0931075bd
uyanikm57 <98463083+uyanikm57@users.noreply.github.com>
2023-07-14 01:07:45 +0300

    Update periodic_table.sql

--- Periodic Table Database/periodic_table.sql
+++ Periodic Table Database/periodic_table.sql
@@ -16,11 +16,32 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+DROP DATABASE periodic_table;
+--
+-- Name: periodic_table; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE periodic_table WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE periodic_table OWNER TO postgres;
+
+\connect periodic_table
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
-
-\connect periodic_table
 
 --
 -- Name: elements; Type: TABLE; Schema: public; Owner: freecodecamp
@@ -41,7 +62,6 @@ ALTER TABLE public.elements OWNER TO freecodecamp;
 
 CREATE TABLE public.properties (
     atomic_number integer NOT NULL,
-    type character varying(30),
     atomic_mass numeric NOT NULL,
     melting_point_celsius numeric NOT NULL,
     boiling_point_celsius numeric NOT NULL,
@@ -75,7 +95,6 @@ INSERT INTO public.elements VALUES (5, 'B', 'Boron');
 INSERT INTO public.elements VALUES (6, 'C', 'Carbon');
 INSERT INTO public.elements VALUES (7, 'N', 'Nitrogen');
 INSERT INTO public.elements VALUES (8, 'O', 'Oxygen');
-INSERT INTO public.elements VALUES (1000, 'Mt', 'moTanium');
 INSERT INTO public.elements VALUES (9, 'F', 'Fluorine');
 INSERT INTO public.elements VALUES (10, 'Ne', 'Neon');
 
@@ -84,17 +103,16 @@ INSERT INTO public.elements VALUES (10, 'Ne', 'Neon');
 -- Data for Name: properties; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.properties VALUES (3, 'metal', 6.94, 180.54, 1342, 1);
-INSERT INTO public.properties VALUES (4, 'metal', 9.0122, 1287, 2470, 1);
-INSERT INTO public.properties VALUES (1, 'nonmetal', 1.008, -259.1, -252.9, 2);
-INSERT INTO public.properties VALUES (2, 'nonmetal', 4.0026, -272.2, -269, 2);
-INSERT INTO public.properties VALUES (6, 'nonmetal', 12.011, 3550, 4027, 2);
-INSERT INTO public.properties VALUES (7, 'nonmetal', 14.007, -210.1, -195.8, 2);
-INSERT INTO public.properties VALUES (8, 'nonmetal', 15.999, -218, -183, 2);
-INSERT INTO public.properties VALUES (5, 'metalloid', 10.81, 2075, 4000, 3);
-INSERT INTO public.properties VALUES (1000, 'metalloid', 1, 10, 100, 3);
-INSERT INTO public.properties VALUES (9, NULL, 18.998, -220, -188.1, 2);
-INSERT INTO public.properties VALUES (10, NULL, 20.18, -248.6, -246.1, 2);
+INSERT INTO public.properties VALUES (3, 6.94, 180.54, 1342, 1);
+INSERT INTO public.properties VALUES (4, 9.0122, 1287, 2470, 1);
+INSERT INTO public.properties VALUES (1, 1.008, -259.1, -252.9, 2);
+INSERT INTO public.properties VALUES (2, 4.0026, -272.2, -269, 2);
+INSERT INTO public.properties VALUES (6, 12.011, 3550, 4027, 2);
+INSERT INTO public.properties VALUES (7, 14.007, -210.1, -195.8, 2);
+INSERT INTO public.properties VALUES (8, 15.999, -218, -183, 2);
+INSERT INTO public.properties VALUES (5, 10.81, 2075, 4000, 3);
+INSERT INTO public.properties VALUES (9, 18.998, -220, -188.1, 2);
+INSERT INTO public.properties VALUES (10, 20.18, -248.6, -246.1, 2);
 
 
 --
